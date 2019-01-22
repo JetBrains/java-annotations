08807b2e14c30c8e232f974811b73ee7d332a729
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2019-01-22 10:53:48 +0700

    [#8] Publish Range annotation

---
+++ java8/src/main/java/org/jetbrains/annotations/Range.java
@@ -0,0 +1,30 @@
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * An annotation which allows to specify for integral type (byte, char, short, int, long) an allowed values range.
+ * Applying this annotation to other types is not correct.
+ * <p>
+ * Example:
+ * <pre>{@code public @Range(from = 0, to = Integer.MAX_VALUE) int length() { 
+ *   return this.length; // returns a non-negative integer
+ * }}</pre>
+ * </p>
+ * 
+ * @since 17.0.0
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE_USE})
+public @interface Range {
+  /**
+   * @return minimal allowed value (inclusive)
+   */
+  long from();
+
+  /**
+   * @return maximal allowed value (inclusive)
+   */
+  long to();
+}
