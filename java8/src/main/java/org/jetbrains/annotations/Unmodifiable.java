c7ca7ebc00f35a787799113c03e9ab58f949c64f
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-01-24 15:02:17 +0700

    Unmodifiable & UnmodifiableView

---
+++ java8/src/main/java/org/jetbrains/annotations/Unmodifiable.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * An annotation which marks a {@link java.util.Collection} or {@link java.util.Map} type
+ * as unmodifiable. A collection or a map is unmodifiable if any mutator methods
+ * throw exception or have no effect, and the objects stored as collection elements,
+ * map keys, and map values are never changed.
+ * 
+ * @see UnmodifiableView
+ * @since 19.0.0
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE_USE})
+public @interface Unmodifiable {
+}
