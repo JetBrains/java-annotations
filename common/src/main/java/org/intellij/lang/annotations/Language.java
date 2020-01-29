17f4789e91f73c90d6d7eca0e6abaacf11f89362
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-01-29 15:19:10 +0700

    Make Nls/NonNls TYPE_USE

--- common/src/main/java/org/intellij/lang/annotations/Language.java
+++ common/src/main/java/org/intellij/lang/annotations/Language.java
@@ -16,8 +16,6 @@
 
 package org.intellij.lang.annotations;
 
-import org.jetbrains.annotations.NonNls;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -28,8 +26,8 @@ import static java.lang.annotation.ElementType.*;
 @Target({ METHOD, FIELD, PARAMETER, LOCAL_VARIABLE, ANNOTATION_TYPE })
 
 public @interface Language {
-    @NonNls String value();
+    String value();
 
-    @NonNls String prefix() default "";
-    @NonNls String suffix() default "";
+    String prefix() default "";
+    String suffix() default "";
 }
