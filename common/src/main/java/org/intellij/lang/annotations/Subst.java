05bb52a1dd46aca9dd00bf2a0334b95c43b10471
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-03-26 12:43:38 +0700

    Subst: javadoc; retention specified

--- common/src/main/java/org/intellij/lang/annotations/Subst.java
+++ common/src/main/java/org/intellij/lang/annotations/Subst.java
@@ -16,6 +16,32 @@
 
 package org.intellij.lang.annotations;
 
+import java.lang.annotation.*;
+
+/**
+ * Specifies the replacement value for non-constant variables and method return values.
+ * This may help static analyzers to properly parse the concatenation of several values
+ * which is used in @{@link Language} or {@link Pattern} context.
+ * <p>
+ * Example:
+ * <pre>
+ * &#64;Subst("Tahoma")
+ * final String font = new JLabel().getFont().getName();
+ * 
+ * &#64;Language("HTML")
+ * String message = "&lt;html&gt;&lt;span style='font: " + font + "; font-size:smaller'&gt;" 
+ *   + ... + "&lt;/span&gt;&lt;/html&gt;";
+ * </pre>
+ * <p>
+ * Here the parser assumes that when {@code font} appears in the concatenation it's value is {@code "Tahoma"},
+ * so it can continue parsing the concatenation.
+ * </p>
+ * 
+ * @see Language
+ * @see Pattern
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.PARAMETER})
 public @interface Subst {
     String value();
 }
