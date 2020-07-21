f9463dd7885150f8f5ecfa1f55bb0bf5009f9de5
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-07-21 16:41:30 +0700

    Nullable value @NonNls

--- java5/src/main/java/org/jetbrains/annotations/Nullable.java
+++ java5/src/main/java/org/jetbrains/annotations/Nullable.java
@@ -38,5 +38,5 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
 public @interface Nullable {
-  String value() default "";
+  @NonNls String value() default "";
 }
