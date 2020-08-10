d1584076ef8e72d571966d55f7743852de417d06
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-08-10 16:58:04 +0700

    NonNls: TYPE_USE annotation

--- common/src/main/java/org/jetbrains/annotations/NonNls.java
+++ common/src/main/java/org/jetbrains/annotations/NonNls.java
@@ -60,7 +60,7 @@ import java.lang.annotation.*;
  */
 @Documented
 @Retention(RetentionPolicy.CLASS)
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE, ElementType.PACKAGE})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE, ElementType.TYPE, ElementType.PACKAGE})
 public @interface NonNls {
 
 }
