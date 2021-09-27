9642ffd9a44e34c261a45a040adae2cc5e1cf216
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2021-09-27 13:58:13 +0300

    [Non]BlockingContext annotations introduced

---
+++ java8/src/main/java/org/jetbrains/annotations/NonBlockingContext.java
@@ -0,0 +1,12 @@
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Indicates that the annotated execution context (e.g. coroutine dispatcher, scheduler, etc.) does not allow blocking calls inside.
+ */
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE_USE})
+public @interface NonBlockingContext {
+}
