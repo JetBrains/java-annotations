33730df842d4aae5055566e1f040d49fc59eb502
Sergei Sysoev <Sergey.Sysoev@jetbrains.com>
2024-05-08 17:18:31 +0200

    Rename JPMS module for multiplatform annotations to "org.jetbrains.annotations.multiplatform"

--- multiplatform-module-info/src/main/java/module-info.java
+++ multiplatform-module-info/src/main/java/module-info.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-module org.jetbrains.multiplatform_annotations {
+module org.jetbrains.annotations.multiplatform {
   requires static java.desktop;
   exports org.intellij.lang.annotations;
   exports org.jetbrains.annotations;
